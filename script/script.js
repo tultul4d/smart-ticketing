2baea9fc8894d02f14e32ec95d4d792e1b170a2e
tultul <tultulakter963@gmail.com>
2024-02-19 07:12:52 +0600

    added tikect count

--- script/script.js
+++ script/script.js
@@ -5,13 +5,43 @@ let totalPriceCount=0;
 let clickCount =0;
 
 for(let bed of allBed){
-    bed.addEventListener('click', function(){
-        bed.style.backgroundColor = "#1DD100";
+    bed.addEventListener('click', function(e){
+
+
+        if ( countSeat < 4){
+            countSeat = countSeat + 1;
+            bed.style.backgroundColor = "#1DD100";
+            setInnerText('bed-count', countSeat);
+            const bedAdd = document.getElementById('bed-count');
+            const bedText = parseInt(bedAdd.innerText);
+            bedAdd.innerText = countSeat;
+
+
+
+
+            let bedNumber = document.getElementById('bed-number');
+            const seatSubText = parseInt(bedNumber.innerText);
+            seatLeft = seatLeft -1;
+            setInnerText('bed-number', seatLeft)
+
+            bedNumber.innerText = seatLeft;
+
+            // const placeTicket =e.target;
+            // console.log(e.target);
+        }
+        else{
+            countSeat = countSeat;
+        }
+        
         // seat.setAttributes('disabled', ' ');
-        let bedAdd = document.getElementById('bed-count');
-        let bedText = parseInt(bedAdd.innerText);
-        countSeat += 1;
-        bedAdd.innerText = countSeat;
+        
+        
+        
+        
     
     })
+}
+function setInnerText(id, value){
+    const setText = document.getElementById(id);
+    setText.innerText = value;
 }
